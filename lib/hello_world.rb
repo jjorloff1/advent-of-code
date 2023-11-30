1908a571406a2fdc1e57c681317858677cedb77b
Jesse Orloff <jesse.orloff@daveramsey.com>
2023-11-30 08:49:00 -0600

    Create initial hello world script.

---
+++ lib/hello_world.rb
@@ -0,0 +1,10 @@
+# hello_world.rb
+
+class HelloWorld
+    def self.say_hello
+      'Hello, World!'
+    end
+  end
+  
+  puts HelloWorld.say_hello
+  
---
+++ spec/hello_world_spec.rb
@@ -0,0 +1,11 @@
+# spec/hello_world_spec.rb
+
+require_relative '../lib/hello_world'
+
+RSpec.describe HelloWorld do
+  describe '.say_hello' do
+    it 'returns the correct greeting' do
+      expect(HelloWorld.say_hello).to eq('Hello, World!')
+    end
+  end
+end
