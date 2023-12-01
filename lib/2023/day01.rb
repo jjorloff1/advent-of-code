764b022d522bf704a35d2ff7f1804db6089e716d
Jesse Orloff <jesse.orloff@daveramsey.com>
2023-12-01 13:38:14 -0600

    Solve 2023 Day 1, part 2.
    Minor refactorings.

--- lib/2023/day01.rb
+++ lib/2023/day01.rb
@@ -14,13 +14,40 @@ end
 
 # This takes a line and provides the calibration value
 class Line
+  DIGIT_TO_WORD = {
+    'one' => 1,
+    'two' => 2,
+    'three' => 3,
+    'four' => 4,
+    'five' => 5,
+    'six' => 6,
+    'seven' => 7,
+    'eight' => 8,
+    'nine' => 9
+  }.freeze
+
+  FIRST_DIGIT_REGEX = /^\D*?(\d|one|two|three|four|five|six|seven|eight|nine)/
+  # Reverse regex for matching a reversed string
+  LAST_DIGIT_REGEX = /^\D*?(\d|enin|thgie|neves|xis|evif|ruof|eerht|owt|eno)/
+
   def initialize(line)
     @line = line
   end
 
   def calibration_value
-    calibration_string = @line[/^\D*?(\d)/, 1] + @line[/(\d)\D*?$/, 1]
+    first_digit = digit_value(@line[FIRST_DIGIT_REGEX, 1])
+    # Rather than figure out a pretty complex regex to get the last number, I decided to use the
+    # easy first number regex by reversing the strings
+    last_digit = digit_value(@line.reverse[LAST_DIGIT_REGEX, 1].reverse)
+
+    calibration_string = first_digit.to_s + last_digit.to_s
     calibration_string.to_i
+  end
+
+  private
+
+  def digit_value(digit_string)
+    (DIGIT_TO_WORD[digit_string].nil? ? digit_string.to_i : DIGIT_TO_WORD[digit_string])
   end
 end
 
--- spec/2023/day01_spec.rb
+++ spec/2023/day01_spec.rb
@@ -5,23 +5,24 @@
 require_relative '../../lib/2023/day01'
 
 RSpec.describe Day01 do
-  let(:long_input) { "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet" }
-  let(:short_input) { "1abc2\npqr3stu8vwx" }
+  shared_examples 'a document calibration' do |document, expected_value|
+    let(:file) { instance_double('file') }
+    before { allow(File).to receive(:open).with('input/2023/01/input.txt').and_return(file) }
+
+    it 'provides the calibration value' do
+      allow(file).to receive(:read).and_return(document)
+      expect(described_class.solve).to eq(expected_value)
+    end
+  end
 
   describe Day01 do
     let(:file) { instance_double('file') }
 
     before { allow(File).to receive(:open).with('input/2023/01/input.txt').and_return(file) }
 
-    it 'provides a calibration value for an input document' do
-      allow(file).to receive(:read).and_return(long_input)
-      expect(described_class.solve).to eq(142)
-    end
-
-    it 'provides a calibration value for an input document with different values' do
-      allow(file).to receive(:read).and_return(short_input)
-      expect(described_class.solve).to eq(50)
-    end
+    it_behaves_like 'a document calibration', "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet", 142
+    it_behaves_like 'a document calibration', "1abc2\npqr3stu8vwx", 50
+    it_behaves_like 'a document calibration', "two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen", 281
   end
 
   shared_examples 'a line calibration value' do |line, expected_value|
@@ -36,20 +37,24 @@ RSpec.describe Day01 do
     it_behaves_like 'a line calibration value', 'pqr3stu8vwx', 38
     it_behaves_like 'a line calibration value', 'a1b2c3d4e5f', 15
     it_behaves_like 'a line calibration value', 'treb7uchet', 77
-
-    xit 'returns 0 nothing when there are no numbers'
+    it_behaves_like 'a line calibration value', 'two1nine', 29
+    it_behaves_like 'a line calibration value', 'eightwothree', 83
+    it_behaves_like 'a line calibration value', 'abcone2threexyz', 13
+    it_behaves_like 'a line calibration value', 'xtwone3four', 24
+    it_behaves_like 'a line calibration value', '4nineeightseven2', 42
+    it_behaves_like 'a line calibration value', 'zoneight234', 14
+    it_behaves_like 'a line calibration value', '7pqrstsixteen', 76
   end
 
   describe Calibrator do
     it 'adds the calibration values of all lines' do
-      calibrator = described_class.new(long_input)
+      calibrator = described_class.new("1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet")
       expect(calibrator.calibrate_document).to eq(142)
     end
 
     it 'adds the calibration values of fewer lines' do
-      calibrator = described_class.new(short_input)
+      calibrator = described_class.new("1abc2\npqr3stu8vwx")
       expect(calibrator.calibrate_document).to eq(50)
     end
   end
-
 end
