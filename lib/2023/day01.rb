54bb902723fc3d3c63f345228d35dc15a768d501
Jesse Orloff <jesse.orloff@daveramsey.com>
2023-12-01 10:19:20 -0600

    Solve 2023 Day 1, part 1.

--- lib/2023/day01.rb
+++ lib/2023/day01.rb
@@ -4,9 +4,37 @@
 
 # This is to solve the Day 1 problem for Advent of Code
 class Day01
-  def self.say_hello
-    'Hello, World!'
+  def self.solve
+    file = File.open('input/2023/01/input.txt')
+    calibration_document = file.read
+
+    Calibrator.new(calibration_document).calibrate_document
   end
 end
 
-puts Day01.say_hello
+# This takes a line and provides the calibration value
+class Line
+  def initialize(line)
+    @line = line
+  end
+
+  def calibration_value
+    calibration_string = @line[/^\D*?(\d)/, 1] + @line[/(\d)\D*?$/, 1]
+    calibration_string.to_i
+  end
+end
+
+# Calibrator takes a calibration document, and provides the calibration value
+class Calibrator
+  def initialize(calibration_document)
+    @calibration_document = calibration_document
+  end
+
+  def calibrate_document
+    @calibration_document.lines
+                         .map { |line| Line.new(line).calibration_value }
+                         .sum
+  end
+end
+
+puts Day01.solve
--- spec/2023/day01_spec.rb
+++ spec/2023/day01_spec.rb
@@ -5,9 +5,51 @@
 require_relative '../../lib/2023/day01'
 
 RSpec.describe Day01 do
-  describe '.say_hello' do
-    it 'returns the correct greeting' do
-      expect(described_class.say_hello).to eq('Hello, World!')
+  let(:long_input) { "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet" }
+  let(:short_input) { "1abc2\npqr3stu8vwx" }
+
+  describe Day01 do
+    let(:file) { instance_double('file') }
+
+    before { allow(File).to receive(:open).with('input/2023/01/input.txt').and_return(file) }
+
+    it 'provides a calibration value for an input document' do
+      allow(file).to receive(:read).and_return(long_input)
+      expect(described_class.solve).to eq(142)
+    end
+
+    it 'provides a calibration value for an input document with different values' do
+      allow(file).to receive(:read).and_return(short_input)
+      expect(described_class.solve).to eq(50)
     end
   end
+
+  shared_examples 'a line calibration value' do |line, expected_value|
+    it do
+      line = Line.new(line)
+      expect(line.calibration_value).to eq(expected_value)
+    end
+  end
+
+  describe Line do
+    it_behaves_like 'a line calibration value', '1abc2', 12
+    it_behaves_like 'a line calibration value', 'pqr3stu8vwx', 38
+    it_behaves_like 'a line calibration value', 'a1b2c3d4e5f', 15
+    it_behaves_like 'a line calibration value', 'treb7uchet', 77
+
+    xit 'returns 0 nothing when there are no numbers'
+  end
+
+  describe Calibrator do
+    it 'adds the calibration values of all lines' do
+      calibrator = described_class.new(long_input)
+      expect(calibrator.calibrate_document).to eq(142)
+    end
+
+    it 'adds the calibration values of fewer lines' do
+      calibrator = described_class.new(short_input)
+      expect(calibrator.calibrate_document).to eq(50)
+    end
+  end
+
 end
